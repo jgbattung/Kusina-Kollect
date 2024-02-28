2590becfcb0a07aebdba5f07a0d80423b8562d5a
Jireh Battung <jirehgbattung@gmail.com>
2024-02-28 16:39:49 +0800

    add root directory

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "../globals.css";
+import Navbar from "../components/shared/Navbar";
+import Footer from "../components/shared/Footer";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Kusina Kollect",
+  description: "Recipe Hub Webapp made with Next.js",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Navbar />
+        <main className="flex">
+          <section className="w-full flex items-center min-h-screen bg-light-200">
+            <div className="w-full">
+              {children}
+            </div>
+          </section>
+        </main>
+        <Footer />
+      </body>
+    </html>
+  );
+}
