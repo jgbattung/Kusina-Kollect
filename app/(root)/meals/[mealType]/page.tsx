005650b02386e453cffd17b5aa60c0aecd6b2103
Jireh Battung <jirehgbattung@gmail.com>
2024-02-29 20:16:09 +0800

    dynamic rendering of meals pages

---
+++ app/(root)/meals/[mealType]/page.tsx
@@ -0,0 +1,31 @@
+"use client"
+
+import SectionHeader from "@/app/components/shared/SectionHeader";
+import { MEAL_DESCRIPTIONS, MealName, mealLinks } from "@/app/constants/mealTypes";
+import { getMealDescription } from "@/app/utils/mealUtils";
+import { usePathname } from "next/navigation";
+
+const Page = () => {
+  const pathname = usePathname();
+
+  const currentMealType = mealLinks.find(meal => meal.path.includes(pathname as string))
+
+  const description = getMealDescription(currentMealType?.name)
+
+  return (
+    <div>
+      {currentMealType ? (
+        <>
+          <SectionHeader 
+            title={currentMealType.name}
+            description={description}
+          />
+        </>
+      ) : (
+        <p>Meal type not found</p>
+      )}
+    </div>
+  )
+}
+
+export default Page
