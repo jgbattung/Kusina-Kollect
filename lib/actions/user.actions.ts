360e27c15490c516e53f048b909dff9f7fb061eb
Jireh Battung <jirehgbattung@gmail.com>
2024-03-05 18:32:42 +0800

    create user action for updating/creating and fetching user

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,48 @@
+"use serve"
+
+import { revalidatePath } from "next/cache";
+import User from "../models/user.model";
+import { connectToDB } from "../mongoose"
+
+interface UpdateUserParams {
+  userId: string;
+  username: string;
+  name: string;
+  image: string;
+  path: string;
+}
+
+export async function updateUser({
+  userId,
+  username,
+  name,
+  image,
+  path
+}: UpdateUserParams): Promise<void> {
+  connectToDB();
+  try {
+    await User.findOneAndUpdate(
+      { id: userId },
+      {
+        username,
+        name,
+        image,
+        onboarded: true,
+      },
+      { upsert: true }
+    )
+    revalidatePath(path);
+  } catch (error: any) {
+    throw new Error(`Failed to create/update user: ${error.message}`)
+  }
+};
+
+export async function fetchUser(userId: string) {
+  connectToDB();
+
+  try {
+    return await User.findOne({ id: userId })
+  } catch (error: any) {
+    throw new Error(`Failed to fetch user: ${error.message}`);
+  }
+};
