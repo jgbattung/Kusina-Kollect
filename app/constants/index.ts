ff334252c0502b3f83fa6e78e106b5cbfaa48693
Jireh Battung <jirehgbattung@gmail.com>
2024-03-07 20:51:51 +0800

    create profile tabs routes

--- app/constants/index.ts
+++ app/constants/index.ts
@@ -4,3 +4,9 @@ export const navbarRoutes: { name: string; path: string }[] = [
   { name: 'Ingredients', path: '/ingredients' },
   { name: 'About Us', path: '/about-us' },
 ]
+
+export const profileRoutes: { name: string }[] = [
+  { name: 'Personal Info' },
+  { name: 'Saved Items & Colections' },
+  { name: 'My Recipes' },
+]
