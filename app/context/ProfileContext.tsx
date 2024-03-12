d880a41b5a6b36f7c5e0c845fdd8ff3af5a5115f
Jireh Battung <jirehgbattung@gmail.com>
2024-03-12 22:58:16 +0800

    create profile context

---
+++ app/context/ProfileContext.tsx
@@ -0,0 +1,18 @@
+"use client"
+
+import { createContext, useState } from "react"
+
+export const ProfileContext = createContext({
+  selectedTab: '',
+  setSelectedTab: (tab: string) => {}
+})
+
+export const ProfileProvider = ({ children }: any) => {
+  const [selectedTab, setSelectedTab] = useState('');
+
+  return (
+    <ProfileContext.Provider value={{ selectedTab, setSelectedTab }}>
+      {children}
+    </ProfileContext.Provider>
+  )
+};
