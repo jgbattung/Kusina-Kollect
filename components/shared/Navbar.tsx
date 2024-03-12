0b2d3e09c2277fe2d6e61875e0798e0a03f9ad61
Jireh Battung <jirehgbattung@gmail.com>
2024-03-12 21:41:50 +0800

    add navigation to my account

--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -5,6 +5,9 @@ import Link from "next/link"
 import SearchBar from "./SearchBar"
 import { navbarRoutes } from "@/app/constants"
 import { usePathname } from "next/navigation"
+import { NavigationMenu, NavigationMenuContent, NavigationMenuItem, NavigationMenuLink, NavigationMenuList } from "../ui/navigation-menu"
+import { NavigationMenuTrigger } from "@radix-ui/react-navigation-menu"
+import { SignOutButton, SignedIn } from "@clerk/nextjs"
 
 function Navbar() {
   const currentPathname = usePathname();
@@ -21,15 +24,41 @@ function Navbar() {
           />
         </Link>
         <SearchBar />
-        <div className="flex items-center gap-2">
-          <Image 
-            src="/assets/profile-icon-default.png"
-            alt="Profile icon"
-            width={25}
-            height={25}
-          />
-          <p className="max-md:hidden">My Account</p>
-        </div>
+        <SignedIn>
+          <NavigationMenu>
+            <NavigationMenuList>
+              <NavigationMenuItem>
+                <NavigationMenuTrigger>
+                  <div className="flex items-center gap-2">
+                    <Image 
+                      src="/assets/profile-icon-default.png"
+                      alt="Profile icon"
+                      width={25}
+                      height={25}
+                    />
+                    <p className="max-md:hidden">My Account</p>
+                    <span className="text-xs font-light">▼</span>
+                  </div>
+                </NavigationMenuTrigger>
+                <NavigationMenuContent>
+                  <NavigationMenuLink>
+                    <ul className="min-w-52 bg-white text-sm font-medium ">
+                      <li className="p-4 hover:bg-accent-500">
+                        <Link href="/profile">My Profile</Link>
+                      </li>
+                      <li className="p-4 hover:bg-accent-500">
+                        <Link href="/">Add a recipe</Link>
+                      </li>
+                      <li className="p-4 hover:bg-accent-500">
+                        <SignOutButton>Log out</SignOutButton>
+                      </li>
+                    </ul>
+                  </NavigationMenuLink>
+                </NavigationMenuContent>
+              </NavigationMenuItem>
+            </NavigationMenuList>
+          </NavigationMenu>
+        </SignedIn>
       </div>
       <nav className="max-lg:hidden pt-2 flex flex-1 items-center justify-evenly">
         <div>
