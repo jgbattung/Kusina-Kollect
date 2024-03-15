2994ff311f1b3564dae7986dc9e019ad6df382e1
Jireh Battung <jirehgbattung@gmail.com>
2024-03-16 00:51:43 +0800

    add custom error message to user validation

--- lib/validations/user.ts
+++ lib/validations/user.ts
@@ -1,7 +1,9 @@
 import * as z from 'zod'
 
 export const UserValidation = z.object({
-  username: z.string().min(1).max(30),
-  name: z.string().min(0).max(70),
+  username: z.string()
+    .min(1, { message: 'Username is required.' })
+    .max(30, { message: 'Username must be 30 characters or fewer.' }),
+  name: z.string().min(0).max(70, { message: 'Name must be 70 characters or fewer.' }),
   profile_photo: z.string().url().min(1),
 })
