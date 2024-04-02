aada4dc09046278a122ce3d9e2609450f918388d
Jireh Battung <jirehgbattung@gmail.com>
2024-04-02 13:26:17 +0800

    new recipe componenet

---
+++ components/shared/NewRecipes.tsx
@@ -0,0 +1,27 @@
+import { fetchNewRecipes } from "@/lib/actions/recipe.actions"
+import Link from "next/link";
+import RecipeCard from "../cards/RecipeCard";
+
+async function NewRecipes() {
+  const newRecipes = await fetchNewRecipes();
+  console.log(newRecipes)
+
+  return (
+    <section className="page-container flex flex-col gap-8">
+      <p className="heading-large">{`What's New?`}</p>
+      <div className="grid grid-cols-3 gap-20 max-lg:grid-cols-2 max-lg:gap-10 max-md:grid-cols-1">
+        {newRecipes.map((recipe) => (
+          <Link href="/" key={recipe.id}>
+            <RecipeCard 
+              id={recipe._id}
+              name={recipe.name}
+              images={recipe.images}
+            />
+          </Link>
+        ))}
+      </div>
+    </section>
+  )
+}
+
+export default NewRecipes
