f2cbd6f1e5a4dabe969cc588b8ad78275a6d2b80
Jireh Battung <jirehgbattung@gmail.com>
2024-04-03 13:03:54 +0800

    create date util

--- lib/utils.ts
+++ lib/utils.ts
@@ -9,3 +9,9 @@ export function isBase64Image(imageData: string) {
   const base64Regex = /^data:image\/(png|jpe?g|gif|webp);base64,/;
   return base64Regex.test(imageData);
 }
+
+export function formatDate(dateInput: Date | string): string {
+  const date = new Date(dateInput);
+  const options: Intl.DateTimeFormatOptions = { year: 'numeric', month: 'long', day:'numeric' }
+  return date.toLocaleDateString('en-US', options);
+}
