f39eb202a7264bec66bf0ac3fe2f7b7669a97c01
Jireh Battung <jirehgbattung@gmail.com>
2024-04-12 19:00:28 +0800

    create table for all recipes and users

--- app/(admin)/admin-panel/page.tsx
+++ app/(admin)/admin-panel/page.tsx
@@ -1,6 +1,8 @@
 
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import { getAllRecipes, getAllUsers } from '@/lib/actions/admin.actions';
 import { fetchUser } from '@/lib/actions/user.actions';
+import { formatDate } from '@/lib/utils';
 import { currentUser } from '@clerk/nextjs';
 import Link from 'next/link';
 import { redirect } from 'next/navigation';
@@ -10,6 +12,9 @@ const AdminPanel  = async () => {
   const user = await currentUser()
   if (!user) redirect('/sign-in');
   const userData = await fetchUser(user.id)
+
+  const allRecipes = await getAllRecipes();
+  const allUsers = await getAllUsers();
 
   if(!userData.isAdmin) {
     return (
@@ -27,7 +32,7 @@ const AdminPanel  = async () => {
 
 
   return (
-    <div className='page-container mx-10'>
+    <div className='page-container my-10 mx-20'>
       <h1 className='heading-bold'>Administration</h1>
       <div className='w-full'>
         <Tabs defaultValue='recipes'>
@@ -36,10 +41,47 @@ const AdminPanel  = async () => {
             <TabsTrigger value='users'>Users</TabsTrigger>
           </TabsList>
           <TabsContent value='recipes'>
-            <table>
-              <tr>
-                <th></th>
-              </tr>
+            <table className='table-auto w-full text-left'>
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>Status</th>
+                  <th>Submitted By</th>
+                  <th>Created At</th>
+                </tr>
+              </thead>
+              <tbody>
+                {allRecipes.map((recipe, index) => (
+                  <tr key={index}>
+                    <td>{recipe._id}</td>
+                    <td>{`${recipe.isApproved ? 'Approved' : 'Not Approved'}`}</td>
+                    <td>{recipe.submittedBy.name ? recipe.submittedBy.name : recipe.submittedBy.username}</td>
+                    <td>{formatDate(recipe.createdAt)}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </TabsContent>
+          <TabsContent value='users'>
+            <table className='table-auto w-full text-left'>
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>Account Status</th>
+                  <th>Name</th>
+                  <th>Username</th>
+                </tr>
+              </thead>
+              <tbody>
+                {allUsers.map((user, index) => (
+                  <tr key={index}>
+                    <td>{user.id}</td>
+                    <td>{user.isContributor ? 'Contributor' : 'User'}</td>
+                    <td>{user.name ? user.name : ''}</td>
+                    <td>{user.username ? user.username : ''}</td>
+                  </tr>
+                ))}
+              </tbody>
             </table>
           </TabsContent>
         </Tabs>
