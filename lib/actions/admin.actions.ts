785d54f4aefd8d865f97ba84223da7a08128736a
Jireh Battung <jirehgbattung@gmail.com>
2024-04-12 18:59:47 +0800

    create action for fething all users

--- lib/actions/admin.actions.ts
+++ lib/actions/admin.actions.ts
@@ -1,6 +1,7 @@
 "use server"
 
 import Recipe from "../models/recipe.model";
+import User from "../models/user.model";
 import { connectToDB } from "../mongoose"
 
 export async function getAllRecipes() {
@@ -17,5 +18,19 @@ export async function getAllRecipes() {
     return alllRecipes;
   } catch (error: any) {
     throw new Error(`Failed to fetch recipes: ${error.message}`)
+  }
+}
+
+export async function getAllUsers() {
+  connectToDB();
+
+  try {
+    const users = await User.find()
+      .select('id image isContributor name username')
+      .exec();
+
+    return users;
+  } catch (error: any) {
+    throw new Error(`Failed to fetch all users: ${error.message}`)
   }
 }
