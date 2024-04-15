eb216832ec28dc1c99b8f70a0b62f017f5129249
Jireh Battung <jirehgbattung@gmail.com>
2024-04-15 14:13:20 +0800

    fix table style

--- app/(admin)/admin-panel/page.tsx
+++ app/(admin)/admin-panel/page.tsx
@@ -35,28 +35,34 @@ const AdminPanel  = async () => {
     <div className='page-container my-10 mx-20'>
       <h1 className='heading-bold'>Administration</h1>
       <div className='w-full'>
-        <Tabs defaultValue='recipes'>
-          <TabsList>
-            <TabsTrigger value='recipes'>Recipes</TabsTrigger>
-            <TabsTrigger value='users'>Users</TabsTrigger>
+        <Tabs defaultValue='recipes' className='w-full'>
+          <TabsList className='flex min-h-14 items-center justify-start'>
+            <TabsTrigger value='recipes' className='text-lg data-[state=active]:font-bold data-[state=active]:text-primary-800'>Recipes</TabsTrigger>
+            <TabsTrigger value='users' className='text-lg data-[state=active]:font-bold data-[state=active]:text-primary-800'>Users</TabsTrigger>
           </TabsList>
           <TabsContent value='recipes'>
             <table className='table-auto w-full text-left'>
-              <thead>
-                <tr>
-                  <th>ID</th>
-                  <th>Status</th>
-                  <th>Submitted By</th>
-                  <th>Created At</th>
+              <thead className='border border-black border-x-black border-t-black border-b-2'>
+                <tr className='bg-accent-500'>
+                  <th scope='col' className='px-4 py-3'>ID</th>
+                  <th scope='col' className='px-4 py-3'>Recipe Name</th>
+                  <th scope='col' className='px-4 py-3'>Status</th>
+                  <th scope='col' className='px-4 py-3'>Submitted By</th>
+                  <th scope='col' className='px-4 py-3'>Created At</th>
+                  <th></th>
                 </tr>
               </thead>
-              <tbody>
+              <tbody className=''>
                 {allRecipes.map((recipe, index) => (
-                  <tr key={index}>
-                    <td>{recipe._id}</td>
-                    <td>{`${recipe.isApproved ? 'Approved' : 'Not Approved'}`}</td>
-                    <td>{recipe.submittedBy.name ? recipe.submittedBy.name : recipe.submittedBy.username}</td>
-                    <td>{formatDate(recipe.createdAt)}</td>
+                  <tr key={index} className='odd:bg-gray-100 even:bg-gray-200 border border-x-black last:border-b-black'>
+                    <td className='px-4 py-3 max-w-24 truncate'>{recipe._id}</td>
+                    <td className='px-4 py-3'>{recipe.name}</td>
+                    <td className='px-4 py-3'>{`${recipe.isApproved ? 'Approved' : 'Not Approved'}`}</td>
+                    <td className='px-4 py-3'>{recipe.submittedBy.name ? recipe.submittedBy.name : recipe.submittedBy.username}</td>
+                    <td className='px-4 py-3'>{formatDate(recipe.createdAt)}</td>
+                    <td className='px-4 py-3'>
+                      <Link href={`/submission/${recipe._id}`} className='text-blue-600 hover:underline'>Check recipe</Link>
+                    </td>
                   </tr>
                 ))}
               </tbody>
@@ -64,21 +70,21 @@ const AdminPanel  = async () => {
           </TabsContent>
           <TabsContent value='users'>
             <table className='table-auto w-full text-left'>
-              <thead>
-                <tr>
-                  <th>ID</th>
-                  <th>Account Status</th>
-                  <th>Name</th>
-                  <th>Username</th>
+              <thead className='border border-black border-x-black border-t-black border-b-2'>
+                <tr className='bg-accent-500'>
+                  <th className='px-4 py-3'>ID</th>
+                  <th className='px-4 py-3'>Account Status</th>
+                  <th className='px-4 py-3'>Name</th>
+                  <th className='px-4 py-3'>Username</th>
                 </tr>
               </thead>
               <tbody>
                 {allUsers.map((user, index) => (
-                  <tr key={index}>
-                    <td>{user.id}</td>
-                    <td>{user.isContributor ? 'Contributor' : 'User'}</td>
-                    <td>{user.name ? user.name : ''}</td>
-                    <td>{user.username ? user.username : ''}</td>
+                  <tr key={index} className='odd:bg-gray-100 even:bg-gray-200 border border-x-black last:border-b-black'>
+                    <td className='px-4 py-3 max-w-24 truncate'>{user._id}</td>
+                    <td className='px-4 py-3 max-w-24'>{user.isContributor ? 'Contributor' : 'User'}</td>
+                    <td className='px-4 py-3'>{user.name ? user.name : ''}</td>
+                    <td className='px-4 py-3'>{user.username ? user.username : ''}</td>
                   </tr>
                 ))}
               </tbody>
