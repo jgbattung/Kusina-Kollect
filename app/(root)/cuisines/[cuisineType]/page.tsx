28e029a2199cac457e2514bd2cd15f7a5c7b0708
Jireh Battung <jirehgbattung@gmail.com>
2024-05-02 16:10:09 +0800

    fetch and display cuisines

--- app/(root)/cuisines/[cuisineType]/page.tsx
+++ app/(root)/cuisines/[cuisineType]/page.tsx
@@ -5,22 +5,59 @@ import SectionLinks from "@/components/shared/SectionLinks";
 import { cuisineLinks } from "@/app/constants/cuisineTypes";
 import { getCuisineDescription } from "@/app/utils/cuisineUtils";
 import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
+import { getRecipesByCuisine } from "@/lib/actions/recipe.actions";
+import RecipeCard from "@/components/cards/RecipeCard";
+
+interface Recipe {
+  _id: string;
+  name: string;
+  images: string[];
+}
 
 const Page = () => {
   const pathname = usePathname();
-
   const currentCuisine = cuisineLinks.find(cuisine => cuisine.path.includes(pathname as string));
+  const [recipes, setRecipes] = useState<Recipe[]>([]);
+
+  useEffect(() => {
+    const fetchRecipes = async () => {
+      if (currentCuisine) {
+        try {
+          const mealRecipes = await getRecipesByCuisine((currentCuisine.name).toLowerCase());
+          setRecipes(mealRecipes);
+        } catch (error) {
+          throw new Error(`Failed to fetch recipes: ${error}`)
+        }
+      }
+    }
+
+    fetchRecipes();
+  }, [currentCuisine])
 
   const description = getCuisineDescription(currentCuisine?.name);
 
   return (
     <div>
       {currentCuisine ? (
-        <SectionHeader 
-          title={currentCuisine.name}
-          description={description}
-          displayImage={currentCuisine.image}
-        />
+        <div className="page-container flex flex-col gap-8">
+          <SectionHeader 
+            title={currentCuisine.name}
+            description={description}
+            displayImage={currentCuisine.image}
+          />
+          <div className="grid grid-cols-3 gap-20 mt-10 max-lg:grid-cols-2 max-lg:gap-10 max-md:grid-cols-1">
+            {recipes.map((recipe: Recipe) => (
+              <RecipeCard 
+                key={recipe._id}
+                id={recipe._id}
+                name={recipe.name}
+                images={recipe.images}
+                category={currentCuisine.name}
+              />
+            ))}
+          </div>
+        </div>
       ) : (
         <>
           <SectionHeader 
