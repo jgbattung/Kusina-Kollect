1db0790572829b54b1a70b016781c3e926d794c3
Jireh Battung <jirehgbattung@gmail.com>
2024-05-10 16:26:39 +0800

    display recipe card

--- components/shared/RecipeOfTheDay.tsx
+++ components/shared/RecipeOfTheDay.tsx
@@ -1,16 +1,38 @@
 import Recipe from '@/lib/models/recipe.model';
 import React from 'react'
+import FeatureRecipeCard from '../cards/FeatureRecipeCard';
 
 interface RecipeOfTheDayProps {
-  recipe: typeof Recipe | null;
+  recipe: {
+    _id: string;
+    name: string;
+    images: string[];
+    submittedBy: {
+      _id: string;
+      image: string;
+      name: string;
+      username: string;
+    };
+    description: string
+  }
 }
 
 function RecipeOfTheDay({ recipe }: RecipeOfTheDayProps) {
   console.log(recipe)
 
   return (
-    <section>
-      <p>Recipe of the Day</p>
+    <section className='w-full bg-white py-12 flex flex-col items-center justify-center gap-7 px-5 max-sm:py-7'>
+      <p className='font-extrabold text-3xl max-sm:font-bold max-sm:text-2xl'>Recipe of the Day</p>
+      {recipe && (
+          <FeatureRecipeCard 
+            id={recipe._id}
+            name={recipe.name}
+            images={recipe.images}
+            description={recipe.description}
+            submittedBy={recipe.submittedBy.name || recipe.submittedBy.username}
+            userImage={recipe.submittedBy.image}
+          />
+      )}
     </section>
   )
 }
