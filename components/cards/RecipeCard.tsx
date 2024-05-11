454638c2723b703c7b80ab03fe236c9b77a42102
Jireh Battung <jirehgbattung@gmail.com>
2024-05-11 20:33:22 +0800

    adjust new recipes rendering

--- components/cards/RecipeCard.tsx
+++ components/cards/RecipeCard.tsx
@@ -10,30 +10,32 @@ interface Props {
 
 const RecipeCard = ({ id, name, images, category }: Props) => {
   return (
-    <article>
-      <Link href={`/recipe/${id}`} className="group flex flex-col gap-3">
-        <div className="relative max-w-80 max-md:min-w-128">
-          {images.length > 0 ? (
-            <Image 
-              src={images[0]}
-              alt={`an image of ${name}`}
-              width={320}
-              height={240}
-              className="max-w-80 min-h-60 max-h-60 object-cover max-lg:min-w-80 max-md:min-w-128 max-sm:min-w-80 max-md:min-h-64"
-            />
-          ) : (
-            <Image 
-              src='/assets/recipe-image.png'
-              alt={`placeholder image for ${name}`}
-              width={320}
-              height={240}
-              className="max-w-80 min-h-60 max-h-60 object-cover max-lg:min-w-80 max-md:min-w-128 max-sm:min-w-80 max-md:min-h-64"
-            />
+    <article className="flex flex-col justify-center items-center">
+      <Link 
+        href={`/recipe/${id}`}
+        className="group flex flex-col gap-2 items-start justify-center"  
+      >
+        {images.length > 0 ? (
+          <Image 
+            src={images[0]}
+            alt={`an image of ${name}`}
+            width={520}
+            height={320}
+            className="px-2 object-cover max-h-72 min-h-72 max-w-90 min-w-90 max-xl:min-w-115 max-xl:min-h-80 max-lg:min-w-105 max-lg:min-h-60 max-[910px]:min-w-75 max-md:min-w-130 max-md:min-h-75 max-sm:min-w-105 max-sm:min-h-70 max-[490px]:min-w-60 max-[490px]:min-h-60"
+          />
+        ) : (
+          <Image 
+            src="assets/recipe-image.png"
+            alt={`placeholder image for ${name}`}
+            width={320}
+            height={240}
+          />
+        )}
+        <div className="text-left px-3">
+          {category && (
+            <p className="text-sm font-semibold text-primary-800">{category.toUpperCase()}</p>
           )}
-        </div>
-        <div className="flex flex-col gap-1">
-            <p className="text-sm font-semibold text-primary-800">{category?.toUpperCase()}</p>
-          <p className="font-extrabold text-2xl group-hover:underline-offset-2 group-hover:underline underline-custom">{name}</p>
+          <p className="font-bold text-2xl max-sm:text-lg group-hover:underline group-hover:underline-offset-2 underline-custom">{name}</p>
         </div>
       </Link>
     </article>
--- components/shared/NewRecipes.tsx
+++ components/shared/NewRecipes.tsx
@@ -6,9 +6,9 @@ async function NewRecipes() {
   const newRecipes = await fetchNewRecipes();
 
   return (
-    <section className="page-container flex flex-col gap-8">
+    <section className="w-full page-container bg-supplementary-200 flex flex-col gap-8">
       <p className="heading-large">{`What's New?`}</p>
-      <div className="grid grid-cols-3 gap-20 max-lg:grid-cols-2 max-lg:gap-10 max-md:grid-cols-1">
+      <div className="grid grid-cols-3 gap-16 max-xl:grid-cols-2 max-xl:gap-10 max-lg:gap-7 max-md:grid-cols-1">
         {newRecipes.map((recipe) => (
           <div key={recipe.id}>
             <RecipeCard 
