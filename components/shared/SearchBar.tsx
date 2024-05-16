90b1594accf7ca45a44273b7c0c00fb6834594ed
Jireh Battung <jirehgbattung@gmail.com>
2024-05-16 17:35:33 +0800

    update searchbar styling

--- components/shared/SearchBar.tsx
+++ components/shared/SearchBar.tsx
@@ -1,10 +1,20 @@
+import path from 'path'
 import React from 'react'
 
 function SearchBar() {
   return (
     <form>
-      <div>
-        <input type='text' placeholder='Find a recipe or ingredient' className='p-3 border rounded-sm' />
+      <div className='flex border border-black rounded-sm'>
+        <input type='text' placeholder='Search here...' className='w-full p-3 focus:outline-none' />
+        <button className='bg-primary-500 py-3 px-4 hover:bg-primary-800 transition-all'>
+          <svg fill="white" height="15px" width="15px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 490.4 490.4">
+            <g>
+              <path d="M484.1,454.796l-110.5-110.6c29.8-36.3,47.6-82.8,47.6-133.4c0-116.3-94.3-210.6-210.6-210.6S0,94.496,0,210.796
+                s94.3,210.6,210.6,210.6c50.8,0,97.4-18,133.8-48l110.5,110.5c12.9,11.8,25,4.2,29.2,0C492.5,475.596,492.5,463.096,484.1,454.796z
+                M41.1,210.796c0-93.6,75.9-169.5,169.5-169.5s169.6,75.9,169.6,169.5s-75.9,169.5-169.5,169.5S41.1,304.396,41.1,210.796z"/>
+            </g>
+          </svg>
+        </button>
       </div>
     </form>
   )
