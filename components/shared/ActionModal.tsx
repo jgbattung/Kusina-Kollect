0d3a0899c475c1e28325a9bd2ee19ebdb1e4ebab
Jireh Battung <jirehgbattung@gmail.com>
2024-05-31 15:38:22 +0800

    create action modal component

---
+++ components/shared/ActionModal.tsx
@@ -0,0 +1,55 @@
+"use client"
+
+import { ActionStatus } from "@/app/constants/actionModal";
+import { useActionModalStore } from "@/lib/store";
+import { useEffect } from "react";
+
+interface ActionModalProps {
+  status: ActionStatus;
+  message: string;
+  onClose? : () => void;
+};
+
+const ActionModal = ({status, message, onClose}: ActionModalProps) => {
+  const { showModal, modalStatus, modalMessage, closeModal } = useActionModalStore();
+
+  useEffect(() => {
+    if (showModal) {
+      const timer = setTimeout(() => {
+        closeModal();
+      }, 10000);
+
+      return () => {
+        clearTimeout(timer);
+      };
+    }
+  }, [showModal, closeModal]);
+
+  const getBgColor = () => {
+    switch (modalStatus) {
+      case ActionStatus.SUCCESS:
+        return 'bg-green-200';
+      case ActionStatus.FAIL:
+        return 'bg-red-200';
+      case ActionStatus.MESSAGE:
+        return 'bg-gray-300';
+      default:
+        return 'bg-gray-300';
+    }
+  };
+
+  return (
+    <div className={`w-96 fixed bottom-5 left-5 border border-gray-400 rounded-xl px-6 py-4 ${getBgColor}`}>
+      <div className="flex items-center justify-between">
+        <p>{modalMessage}</p>
+        <button onClick={closeModal}>
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1} d="M6 18L18 6M6 6l12 12" />
+          </svg>
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default ActionModal
