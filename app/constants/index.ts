4b896cc4aa991de10ffe3778375bff9d4e7a7eb7
Jireh Battung <jirehgbattung@gmail.com>
2024-08-26 15:42:23 +0800

    fix footer

--- app/constants/index.ts
+++ app/constants/index.ts
@@ -2,7 +2,6 @@ export const navbarRoutes: { name: string; path: string }[] = [
   { name: 'Meals', path: '/meals' },
   { name: 'Cuisines', path: '/cuisines' },
   { name: 'Ingredients', path: '/ingredients' },
-  { name: 'About Us', path: '/about-us' },
 ]
 
 export const profileRoutes: { name: string }[] = [
